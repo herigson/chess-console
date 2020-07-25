a20660c43f6f1c23a47e4e16bf7aaab8e8a448cf
Herigson Felipe <felipevbraga@outlook.com>
2020-07-25 18:45:57 -0300

    Handling erros with custom exception

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
+            try { 
             Board board = new Board(8, 8);
 
             board.InsertPiece(new Rook(board, Color.Black), new Position(0, 0));
             board.InsertPiece(new Rook(board, Color.Black), new Position(1, 3));
             board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
+            board.InsertPiece(new King(board, Color.Black), new Position(0, 9));
             Screen.Print(board);
+            }
+            catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- board/Board.cs
+++ board/Board.cs
@@ -19,10 +19,36 @@ namespace board
             return Pieces[line, column];
         }
 
+        public Piece Piece(Position position)
+        {
+            return Pieces[position.Line, position.Column];
+        }
+        public bool ExistsPiece(Position position)
+        {
+            ValidatePosition(position);
+            return Piece(position) != null;
+        }
+
         public void InsertPiece(Piece piece, Position position)
         {
+            if (ExistsPiece(position))
+                throw new BoardException("There is already a piece in that position!");
             Pieces[position.Line, position.Column] = piece;
             piece.Position = position;
         }
+
+        public bool ValidPosition(Position position)
+        {
+            if (position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns)
+                return false;
+            return true;
+        }
+
+        public void ValidatePosition(Position position)
+        {
+            if (!ValidPosition(position))
+                throw new BoardException("Invalid Position!");
+        }
+
     }
 }
